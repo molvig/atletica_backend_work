32a121fa8ce5b0d55ff099bc9a49f118f96e0fbb
molvig <jennie@molvig.se>
2014-07-29 15:53:27 +0200

    tisdag em

--- medlem_ny.php
+++ medlem_ny.php
@@ -1,9 +1,16 @@
 <?php include("inc/db_con.php"); ?>
 <?php include("inc/header.php"); ?>
 	<div class="grid_2">
-  <?php include("inc/menymedlem.php"); ?>
+  <?php include("inc/menymedlem.php"); ?> 
+  </div>
+
+
+
+
+
+
+
   
-</div>
 <div class="grid_5">
   <div class="grid_12">
   <h3>Lägg till ny medlem </h3>
