f994f0e115beefa8d98d5234668abeb8a4a09f69
molvig <jennie@molvig.se>
2014-07-31 21:38:23 +0200

    torsdag 21.38

--- schema_nytt_original_pass.php
+++ schema_nytt_original_pass.php
@@ -96,7 +96,7 @@
 
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-default">Uppdatera</button>
+      <button type="submit" class="btn btn-default">Lägg till nytt pass</button>
     </div>
   </div>
 </form>
